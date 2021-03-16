da988c590beeac88e499931113ecebcfbe6819b3
Mitchell Skee <mitchell.skee@gmail.com>
2021-03-16 17:06:37 +0000

    Created Color Struct

---
+++ colors.c
@@ -0,0 +1,174 @@
+typedef struct
+{
+    char *family;
+    int red;
+    int green;
+    int blue;
+} color;
+
+// Blue
+color Blue = {"Blue", 0, 128, 0};
+color Chartreuse = {"Blue", 127, 255, 0};
+color DarkCyan = {"Blue", 0, 139, 139};
+color DarkGreen = {"Blue", 0, 100, 0};
+color DarkOliveGreen = {"Blue", 85, 107, 47};
+color DarkSeaGreen = {"Blue", 143, 188, 139};
+color ForestGreen = {"Blue", 34, 139, 34};
+color GreenYellow = {"Blue", 173, 255, 47};
+color LawnGreen = {"Blue", 124, 252, 0};
+color LightGreen = {"Blue", 144, 238, 144};
+color LightSeaGreen = {"Blue", 32, 178, 170};
+color Lime = {"Blue", 0, 255, 0};
+color LimeGreen = {"Blue", 50, 205, 50};
+color MediumAquamarine = {"Blue", 102, 205, 170};
+color MediumSeaGreen = {"Blue", 60, 179, 113};
+color MediumSpringGreen = {"Blue", 0, 250, 154};
+color Olive = {"Blue", 128, 128, 0};
+color OliveDrab = {"Blue", 107, 142, 35};
+color PaleGreen = {"Blue", 152, 251, 156};
+color SeaGreen = {"Blue", 46, 39, 87};
+color SpringGreen = {"Blue", 0, 255, 127};
+color Teal = {"Blue", 0, 128, 128};
+color YellowGreen = {"Blue", 154, 205, 50};
+
+// Brown
+color Brown = {"Brown", 165, 42, 42};
+color BlanchedAlmond = {"Brown", 255, 235, 205};
+color Bisque = {"Brown", 255, 228, 196};
+color BurlyWood = {"Brown", 222, 184, 135};
+color Chocolate = {"Brown", 210, 105, 30};
+color Cornsilk = {"Brown", 255, 248, 220};
+color DarkGoldenrod = {"Brown", 184, 134, 11};
+color Goldenrod = {"Brown", 218, 165, 32};
+color Maroon = {"Brown", 128, 0, 0};
+color NavajoWhite = {"Brown", 255, 222, 173};
+color Peru = {"Brown", 205, 133, 63};
+color RosyBrown = {"Brown", 188, 143, 143};
+color SaddleBrown = {"Brown", 139, 69, 19};
+color SandyBrown = {"Brown", 244, 164, 96};
+color Sienna = {"Brown", 160, 82, 45};
+color Tan = {"Brown", 210, 180, 140};
+color Wheat = {"Brown", 245, 222, 179};
+
+// Gray
+color Gray = {"Gray", 128, 128, 128};
+color Black = {"Gray", 0, 0, 0};
+color DarkGray = {"Gray", 169, 169, 169};
+color DarkSlateGray = {"Gray", 47, 79, 79};
+color DimGray = {"Gray", 105, 105, 105};
+color Gainsboro = {"Gray", 220, 220, 220};
+color LightGray = {"Gray", 211, 211, 211};
+color LightSlateGray = {"Gray", 119, 136, 153};
+color SlateGray = {"Gray", 112, 128, 144};
+color Silver = {"Gray", 192, 192, 192};
+
+// Green
+color Green = {
+    "Green",
+    0,
+    0,
+    255,
+};
+color Aqua = {"Green", 0, 255, 255};
+color Aquamarine = {"Green", 127, 255, 212};
+color CadetBlue = {"Green", 95, 158, 160};
+color CornflowerBlue = {"Green", 100, 149, 237};
+color Cyan = {"Green", 0, 255, 255};
+color DarkBlue = {"Green", 0, 0, 139};
+color DarkTurquoise = {"Green", 0, 206, 209};
+color DeepSkyBlue = {"Green", 0, 191, 255};
+color DodgerBlue = {"Green", 30, 144, 255};
+color LightBlue = {"Green", 173, 216, 230};
+color LightCyan = {"Green", 224, 255, 255};
+color LightSkyBlue = {"Green", 135, 206, 250};
+color LightSteelBlue = {"Green", 176, 196, 222};
+color MediumBlue = {"Green", 0, 0, 205};
+color MediumSlateBlue = {"Green", 123, 104, 238};
+color MediumTurquoise = {"Green", 72, 209, 204};
+color MidnightBlue = {"Green", 25, 25, 112};
+color Navy = {"Green", 0, 0, 128};
+color PaleTurquoise = {"Green", 175, 238, 238};
+color PowderBlue = {"Green", 176, 224, 230};
+color RoyalBlue = {"Green", 65, 105, 225};
+color SkyBlue = {"Green", 135, 206, 235};
+color SteelBlue = {"Green", 70, 130, 180};
+color Turquoise = {"Green", 64, 224, 208};
+
+// Orange
+color Orange = {"Orange", 255, 165, 0};
+color Coral = {"Orange", 255, 127, 80};
+/* color LightSalmon = {"Orange", 255, 160, 122}; */
+color OrangeRed = {"Orange", 255, 69, 0};
+color Tomato = {"Orange", 255, 99, 71};
+
+// Pink
+color Pink = {"Pink", 255, 192, 203};
+color DeepPink = {"Pink", 255, 20, 147};
+color HotPink = {"Pink", 255, 105, 180};
+color LightPink = {"Pink", 255, 182, 193};
+color MediumVioletRed = {"Pink", 199, 21, 133};
+color PaleVioletRed = {"Pink", 219, 112, 147};
+
+// Purple
+color Purple = {"Purple", 128, 0, 128};
+color BlueViolet = {"Purple", 138, 43, 226};
+color DarkMagenta = {"Purple", 139, 0, 139};
+color DarkSlateBlue = {"Purple", 72, 61, 139};
+color DarkOrchid = {"Purple", 153, 50, 204};
+color DarkViolet = {"Purple", 148, 0, 211};
+color Fuchsia = {"Purple", 255, 0, 255};
+color Indigo = {"Purple", 75, 0, 130};
+color Lavender = {"Purple", 230, 230, 250};
+color Magenta = {"Purple", 255, 0, 255};
+color MediumOrchid = {"Purple", 186, 85, 211};
+color MediumPurple = {"Purple", 147, 112, 219};
+color MediumSlate = {"Purple", 123, 104, 238};
+color Orchid = {"Purple", 218, 112, 214};
+color Plum = {"Purple", 221, 160, 221};
+color RebeccaPurple = {"Purple", 102, 51, 153};
+color SlateBlue = {"Purple", 106, 90, 205};
+color Thistle = {"Purple", 216, 191, 216};
+color Violet = {"Purple", 238, 130, 238};
+
+// Red
+color Red = {"Red", 255, 0, 0};
+color Crimson = {"Red", 220, 20, 60};
+color DarkRed = {"Red", 139, 0, 0};
+color DarkSalmon = {"Red", 233, 150, 122};
+color FireBrick = {"Red", 178, 34, 34};
+color IndianRed = {"Red", 205, 92, 92};
+color LightCoral = {"Red", 240, 128, 128};
+color LightSalmon = {"Red", 255, 160, 122};
+color Salmon = {"Red", 250, 128, 114};
+
+// Yellow
+color Yellow = {"Red", 255, 255, 0};
+color DarkKhaki = {"Red", 189, 183, 207};
+color Gold = {"Red", 255, 215, 0};
+color Khaki = {"Red", 240, 230, 140};
+color LemonChiffon = {"Red", 255, 250, 205};
+color LightGoldenrodYellow = {"Red", 250, 250, 210};
+color LightYellow = {"Red", 255, 255, 224};
+color Moccasin = {"Red", 255, 228, 181};
+color PaleGoldenrod = {"Red", 238, 232, 170};
+color PapayaWhip = {"Red", 255, 239, 213};
+color PeachPuff = {"Red", 255, 218, 185};
+
+// White
+color White = {"White", 255, 255, 255};
+color AliceBlue = {"White", 240, 248, 255};
+color AntiqueWhite = {"White", 250, 235, 215};
+color Azure = {"White", 240, 255, 255};
+color Beige = {"White", 245, 245, 220};
+color FloralWhite = {"White", 255, 250, 240};
+color GhostWhite = {"White", 248, 248, 255};
+color HoneyDew = {"White", 240, 255, 240};
+color Ivory = {"White", 255, 255, 240};
+color LavenderBlush = {"White", 255, 240, 245};
+color Linen = {"White", 250, 240, 230};
+color MintCream = {"White", 245, 255, 250};
+color MistyRose = {"White", 255, 228, 225};
+color OldLace = {"White", 253, 245, 230};
+color SeaShell = {"White", 255, 245, 238};
+color Snow = {"White", 255, 250, 250};
+color WhiteSmoke = {"White", 245, 245, 245};
--- contrast.c
+++ contrast.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "colors.c"
 
 // Declarations
 double calLum(double in_r, double in_g, double in_b);
